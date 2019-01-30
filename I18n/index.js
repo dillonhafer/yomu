a7e6cda033ce9884f86ee3a13c2c7ad1404826ed
Dillon Hafer <dh@dillonhafer.com>
2019-01-30 16:38:07 -0500

    Fix locale lookup

--- I18n/index.js
+++ I18n/index.js
@@ -2,24 +2,25 @@ import { Localization } from 'expo-localization';
 import i18n from 'i18n-js';
 
 export const locale = Localization.locale;
+export const localeShort = locale.split('-')[0];
 
 const translations = locale => {
   switch (locale) {
-    case 'ja-JP':
+    case 'ja':
       return () => require('./jp.json');
-    case 'en-US':
+    case 'en':
     default:
       return () => require('./en.json');
   }
 };
 
 i18n.fallbacks = true;
-i18n.translations = { [locale]: translations(locale)() };
+i18n.translations = { [locale]: translations(localeShort)() };
 i18n.locale = locale;
 
 export const weekDays = {
-  'ja-JP': ['日', '月', '火', '水', '木', '金', '土'],
-  'en-US': ['S', 'M', 'T', 'W', 'R', 'F', 'S'],
-}[locale];
+  ja: ['日', '月', '火', '水', '木', '金', '土'],
+  en: ['S', 'M', 'T', 'W', 'R', 'F', 'S'],
+}[localeShort];
 
 export default i18n;
